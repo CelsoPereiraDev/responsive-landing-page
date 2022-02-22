1cb728bda4eab193dd4021f1704c4ecb2f901b9b
Celso Pereira <99039824+CelsoPereiraDev@users.noreply.github.com>
2022-02-22 09:26:54 -0300

    Adição do Home e correção nas variáveis  das CSS

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 /*====== MENU SHOW AND HIDDEN =====*/
-const navMenu = document.getElementById('nav-menu')
-    toggleMenu = document.getElementById('nav-toggle')
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
     closeMenu = document.getElementById('nav-close')
 
 // SHOW
 toggleMenu.addEventListenner('click', ()=>{
     navMenu.classList.toggle('show')
-
 })
 
 //HIDDEN
 closeMenu.addEventListenner('click', ()=>{
     navMenu.classList.remove('show')
-
 })
 /*====== MENU SHOW AND HIDDEN =====*/
 
