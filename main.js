8579bc15e818b86efd72ac49566627a27f17d07d
Celso Pereira <99039824+CelsoPereiraDev@users.noreply.github.com>
2022-02-22 08:38:00 -0300

    Inicio do projeto
    Adição de Variáveis das CSS e Header

---
+++ main.js
@@ -0,0 +1,26 @@
+/*====== MENU SHOW AND HIDDEN =====*/
+const navMenu = document.getElementById('nav-menu')
+    toggleMenu = document.getElementById('nav-toggle')
+    closeMenu = document.getElementById('nav-close')
+
+// SHOW
+toggleMenu.addEventListenner('click', ()=>{
+    navMenu.classList.toggle('show')
+
+})
+
+//HIDDEN
+closeMenu.addEventListenner('click', ()=>{
+    navMenu.classList.remove('show')
+
+})
+/*====== MENU SHOW AND HIDDEN =====*/
+
+
+/*====== GSAP ANIMATION =====*/
+
+
+//NAV
+
+
+//HOME
